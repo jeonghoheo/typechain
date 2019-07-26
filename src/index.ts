ea0b3926dffb0ce053dfb7533c43a62f6226d6ca
jeonghoheo <heojeongho1992@gmail.com>
2019-07-26 20:31:35 +0900

    Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are a ${age}, you are a ${gender}`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const person = {
+  name: "Jeongho",
+  age: 123,
+  gender: "male"
 };
 
-console.log(sayHi("Jeongho", 123, "male"));
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are a ${person.age}, you are a ${
+    person.gender
+  }`;
+};
+
+console.log(sayHi(person));
 
 export = {};
