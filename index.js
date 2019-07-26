97b7fb59a75ff39ecb022efe6e3920900d48bcca
jeonghoheo <heojeongho1992@gmail.com>
2019-07-26 19:58:08 +0900

    Types in Typescript 그리고 환경설정

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const name = "Jeongho", age = 24, gender = "male";
-const sayHi = (name, age, gender) => {
-    console.log(`Hello ${name}, you are a ${age}, you are a ${gender}`);
-};
-sayHi(name, age);
-module.exports = {};
-//# sourceMappingURL=index.js.map
