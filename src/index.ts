0a5719fedf36ff9e3b7e935e5bcd59b91e46c37a
jeonghoheo <heojeongho1992@gmail.com>
2019-07-27 11:57:14 +0900

    class로 타입을 지정하기

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,29 @@
-class Human {
-  public name: string;
-  private age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender?: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const choco = new Human("Jeongho", 22);
+const chocoBlock: Block = new Block(0, "2020202020", "", "Hello", 123456);
+const whiteBlock: Block = new Block(2, "4444", "ㅇㄹ", "랄라", 2323);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are a ${person.age}, you are a ${
-    person.gender
-  }`;
-};
+let blockchain: [Block] = [chocoBlock];
 
-console.log(sayHi(choco));
+blockchain.push(whiteBlock);
 
-export = {};
+console.log(blockchain);
